ae26a802b42901c4e696cb7d0d3022b956f50b22
homsau <james.p.cowart@gmail.com>
2018-06-14 15:24:32 -0400

    commit

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
         questions[8] = "Who killed the Prophet of Truth?";
         questions[9] = "Who killed the Prophet of Mercy?";
     var answers = new Array();
-        answers [0] = "The Arbiter,Pickle Juice,Jacob Keyes,Master Chief,Sgt. Major Johnson".split(",");
+        answers [0] = "The Arbiter,Tartarus,Jacob Keyes,Master Chief,Sgt. Major Johnson".split(",");
         answers [1] = "Pillar of Autum,Forward until Dawn,Aegis Fate".split(",");
         answers [2] = "The General,Jacob Keyes,Miranda Keyes,Avery Johnson".split(",");
         answers [3] = "Tatarus,The Arbiter,The Prophet of truth".split(",");
         answers [4] = "The Prophets,The Hunters,The Brutes,The Jackals,The Elites".split(",");
         answers [5] = "5,1,7,9,13".split(",");
         answers [6] = "His daughter,Sgt. Johnson,Master Chief".split(",");
-        answers [7] = "TMaster Chief,he Arbiter,Sgt. Johnson".split(",");
+        answers [7] = "Master Chief,The Arbiter,Sgt. Johnson".split(",");
         answers [8] = "The Flood,The Arbiter,Master Chief,Sgt. Johnson".split(",");
         answers [9] = "Master Chief,Sgt. Johnson,The Arbiter,A flood spore".split(",");
     var correctAnswer = new Array();
@@ -46,32 +46,36 @@ $(document).ready(function() {
     var qWrong = 0;
     var qTime = 0;
 
-    console.log(answers);
-
-    $("#questions").text(questions[i]);
-    for (j = 0; j < answers[i].length; j++) {
-        $('#answers').append("<li><input name='answerRadio' type='radio'>" + answers[i][j] + "</input></li>");
-    }
-    $("#answers").on('click','li',function (){
-        var checkAnswer = $(this).text();
-        if (checkAnswer === correctAnswer[i]) {
-            correct();
-        } else {
-            wrong();
+    $("#startGame").click(function() {
+        $(this).attr("style", "display: none;");  
+        tickTock();
+        $("#questions").text(questions[i]);
+        for (j = 0; j < answers[i].length; j++) {
+            $('#answers').append("<li><input name='answerRadio' type='radio'>" + answers[i][j] + "</input></li>").attr("style", "text-align: left;");
         }
+        $("#answers").on('click','li',function (){
+            var checkAnswer = $(this).text();
+            if (checkAnswer === correctAnswer[i]) {
+                correct();
+            } else {
+                wrong();
+            }
+        });
     });
+
+    /**/
     
-    function nextQuestion() {
+    function question() {
         i++;
-        k++;
+        questionCount++;
         tickTock();
         $("#answers").html("");;
         $("#questions").text(questions[i]);
-        if (k === questions.length) {
-            return;
+        if (questionCount === questions.length) {
+            gameEnd();
         } else {
             for (j = 0; j < answers[i].length; j++) {
-                $('#answers').append("<li><input name='answerRadio' type='radio'>" + answers[i][j] + "</input></li>");
+                $('#answers').append("<li><input name='answerRadio' type='radio'>" + answers[i][j] + "</input></li>").attr("style", "text-align: left;");
             }
         }
     }
@@ -80,36 +84,49 @@ $(document).ready(function() {
         intervalId = setInterval(timerFunction, 1000);
         timerLength = 5;
         function timerFunction() {
-            if (timerLength > 0) {
-                timerLength--;
-                $("#timer").text("<h2>" + timerLength + "</h2>");
-            }
-            else if (timerLength === 0) {
-                //clearInterval(intervalId);
+            if (timerLength === 0) {
+                clearInterval(intervalId);
                 timeUp();
             }
-            $("#timer").html(timerLength);
+            if (timerLength > 0) {
+                timerLength--;
+            }
+            $("#timer").html("Time Left: " + timerLength);
         }
     }
-    tickTock();
 
     function timeUp() {
         qTime++;
-        clearInterval(intervalId);
-        setTimeout(nextQuestion, 5000);
-        $("#answers").html("<br>Time Up!<br><br>The Correct Answer Was " + correctAnswer[i]);
+        //clearInterval(intervalId);
+        setTimeout(question, 5000);
+        $("#answers").html("Time Up!<br><br>The Correct Answer Was " + correctAnswer[i]).attr("style", "text-align: center;");
         console.log("qTime " + qTime);
     }
     function correct () {
         qRight++;
-        setTimeout(nextQuestion, 5000);
-        $("#answers").html("<br>Correct! - Nerd (:");
+        clearInterval(intervalId);
+        setTimeout(question, 5000);
+        $("#answers").html("Correct! - Nerd (:").attr("style", "text-align: center;");
         console.log("qRight " + qRight);
     }
     function wrong() {
         qWrong++;
-        setTimeout(nextQuestion, 5000);
-        $("#answers").html("<br>Wrong!<br><br>The Correct Answer Was " + correctAnswer[i] + " - You Should Play More Halo");
+        clearInterval(intervalId);
+        setTimeout(question, 5000);
+        $("#answers").html("Wrong!<br><br>The Correct Answer Was " + correctAnswer[i] + " - You Should Play More Halo").attr("style", "text-align: center;");
         console.log("qWrong " + qWrong);
+    }
+    function gameEnd() {
+        clearInterval(intervalId);
+        $("#timer").html("Game Over!");
+        $("#questions").html("Here are your results")
+        $("#answers").html(qRight + " correct<br>" + qWrong + " wrong<br>" + qTime + " out of time");
+        if (qRight <= 5) {
+            $("#answers").append("<br>SAD!").attr("style", "text-align: center;");
+        } else if (qRight === questions.length) {
+            $("#answers").append("<br>PERFECT!").attr("style", "text-align: center;");
+        } else {
+        $("#answers").append("<br>Not Bad!").attr("style", "text-align: center;");
+        }
     }
 });
